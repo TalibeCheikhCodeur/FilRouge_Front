88308088c8066db1d89f43e23c88ce54d7bae8c5
TalibeCheikhCodeur <tidjanely99@gmail.com>
2023-10-23 12:40:22 +0000

    last

--- src/app/menu/lister-session/lister-session.component.ts
+++ src/app/menu/lister-session/lister-session.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { isSameDay, isSameMonth } from 'date-fns';
 import { Session, SessionInterface } from 'src/app/session-interface';
 
 @Component({
@@ -12,12 +13,14 @@ export class ListerSessionComponent {
 
   constructor(private http: HttpClient) {
 
-   }
+  }
   // sessions: CalendarEvent[]
   viewDate: Date = new Date();
   view: CalendarView = CalendarView.Week;
   calendarView = CalendarView;
   events: CalendarEvent[] = []
+  activeDayIsOpen = false;
+
   ngOnInit() {
     this.loadSessions();
   }
@@ -28,21 +31,34 @@ export class ListerSessionComponent {
 
   loadSessions() {
     return this.http.get<SessionInterface>("http://127.0.0.1:8000/api/sessions").subscribe((response: SessionInterface) => {
-      // console.log(response);
+
       this.events = this.displayEvent(response.data)
       console.log(response.data);
-       
-   
+
+
     });
   }
 
-  displayEvent(session:Session[]){
-    return session.map((item :Session ) =>({
-      title:item.module,
+  displayEvent(session: Session[]) {
+    return session.map((item: Session) => ({
+      title: item.module,
       start: new Date(item.date + 'T' + item.heure_debut),
-      end:new Date(item.date + 'T' + item.heure_fin),
-      salle:item.salle_id
+      end: new Date(item.date + 'T' + item.heure_fin),
+      salle: item.salle_id
 
     }))
   }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) || events.length===0) {
+        this.activeDayIsOpen = false;
+      }else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date
+    }
+  }
+
 }
